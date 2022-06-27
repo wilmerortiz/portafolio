e3e52def8fcc655437adbc353ea0a4d00bbc2779
wilmerortiz <jhak_dy@hotmail.com>
2022-06-26 19:16:17 -0500

    funcion para generar la edad, se añadió nuevos skills

--- js/script.js
+++ js/script.js
@@ -9,3 +9,19 @@ inputs.forEach((input) => {
     }
   });
 });
+
+function calcularEdad(fecha) {
+  var hoy = new Date();
+  var cumpleanos = new Date(fecha);
+  var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+  var m = hoy.getMonth() - cumpleanos.getMonth();
+
+  if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+    edad--;
+  }
+
+  return edad;
+}
+
+let edad = calcularEdad("1988-11-02");
+document.getElementById("edad").innerHTML = edad;
